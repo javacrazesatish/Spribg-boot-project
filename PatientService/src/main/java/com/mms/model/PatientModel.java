47223adf7c1ea5d93c4a847e5b65fdf6586df64c
javacrazesatish <166551216+javacrazesatish@users.noreply.github.com>
2024-04-10 01:06:11 +0530

    Add files via upload
    patient service initial commit

---
+++ PatientService/src/main/java/com/mms/model/PatientModel.java
@@ -0,0 +1,100 @@
+package com.mms.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "patients_table")
+public class PatientModel {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String name;//required 
+	
+	private LocalDateTime startTime;
+	
+	private LocalDateTime endTime;
+	
+	private String address;//required 
+	
+	private String mobileNo;
+	
+	private String whatappNo;
+	
+	private String adharNo; //required 
+
+	public PatientModel() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDateTime getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(LocalDateTime startTime) {
+		this.startTime = startTime;
+	}
+
+	public LocalDateTime getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(LocalDateTime endTime) {
+		this.endTime = endTime;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getWhatappNo() {
+		return whatappNo;
+	}
+
+	public void setWhatappNo(String whatappNo) {
+		this.whatappNo = whatappNo;
+	}
+
+	public String getAdharNo() {
+		return adharNo;
+	}
+
+	public void setAdharNo(String adharNo) {
+		this.adharNo = adharNo;
+	}
+
+	}
